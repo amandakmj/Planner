f8f254a17d9256a2c674844d7660222a19399aea
Amanda Silva <amandasilvaraujo@outlook.com>
2025-04-20 09:50:08 -0300

    Initial Commit

---
+++ PlannerTests/PlannerTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlannerTests.swift
+//  PlannerTests
+//
+//  Created by Amanda Silva Araujo on 20/04/25.
+//
+
+import Testing
+
+struct PlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
